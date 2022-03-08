88a11d9900442a771edef4b5802ab36f5179b558
CEFgoose <commonwealthgoose@gmail.com>
2022-03-08 10:41:07 -0700

    adjusted icon imports for build, added spec file

--- settings.py
+++ settings.py
@@ -1,5 +1,3 @@
 #---define a few hex colors for easy use
 WHITE = "#FFFFFF"
 BLACK = '#000000'
-VIEW_ICON='static/icons/viewing.png'
-HIDDEN_ICON='static/icons/hidden.png'
--- shapes.py
+++ shapes.py
@@ -12,15 +12,56 @@ def resource_path(relative_path):
     return os.path.join(base_path, relative_path)
 
 #---shapes for editor table & settings previews
-CIRCLE=("static/shapes/circle.png")
-SQUARE=("static/shapes/square.png")
-TRIANGLE =("static/shapes/triangle.png")
-PENTAGON=("static/shapes/pentagon.png")
-HEXAGON =("static/shapes/hexagon.png")
-HEPTAGON=("static/shapes/heptagon.png")
-OCTAGON=("static/shapes/octagon.png")
-NONAGON =("static/shapes/nonagon.png")
-DECAGON=("static/shapes/decagon.png")
+# CIRCLE=("static/shapes/circle.png")
+# SQUARE=("static/shapes/square.png")
+# TRIANGLE =("static/shapes/triangle.png")
+# PENTAGON=("static/shapes/pentagon.png")
+# HEXAGON =("static/shapes/hexagon.png")
+# HEPTAGON=("static/shapes/heptagon.png")
+# OCTAGON=("static/shapes/octagon.png")
+# NONAGON =("static/shapes/nonagon.png")
+# DECAGON=("static/shapes/decagon.png")
+
+# #---sghapes for shape select widget buttons
+# CIRCLEBUTTON=("static/shapes/circleButton.png")
+# SQUAREBUTTON=("static/shapes/squareButton.png")
+# TRIANGLEBUTTON =("static/shapes/triangleButton.png")
+# PENTAGONBUTTON=("static/shapes/pentagonButton.png")
+# HEXAGONBUTTON =("static/shapes/hexagonButton.png")
+# HEPTAGONBUTTON=("static/shapes/heptagonButton.png")
+# OCTAGONBUTTON=("static/shapes/octagonButton.png")
+# NONAGONBUTTON =("static/shapes/nonagonButton.png")
+# DECAGONBUTTON=("static/shapes/decagonButton.png")
+
+
+#---resource paths for images for compiled build
+
+CIRCLE = resource_path('circle.png')
+SQUARE = resource_path('square.png')
+TRIANGLE = resource_path('triangle.png')
+PENTAGON = resource_path('pentagon.png')
+HEXAGON = resource_path('hexagon.png')
+HEPTAGON = resource_path('heptagon.png')
+OCTAGON = resource_path('octagon.png')
+NONAGON = resource_path('nonagon.png')
+DECAGON = resource_path('decagon.png')
+
+CIRCLEBUTTON = resource_path('circleButton.png')
+SQUAREBUTTON = resource_path('squareButton.png')
+TRIANGLEBUTTON = resource_path('triangleButton.png')
+PENTAGONBUTTON = resource_path('pentagonButton.png')
+HEXAGONBUTTON = resource_path('hexagonButton.png')
+HEPTAGONBUTTON = resource_path('heptagonButton.png')
+OCTAGONBUTTON = resource_path('octagonButton.png')
+NONAGONBUTTON = resource_path('nonagonButton.png')
+DECAGONBUTTON = resource_path('decagonButton.png')
+
+VIEW_ICON=resource_path('viewing.png')
+HIDDEN_ICON=resource_path('hidden.png')
+GEM_ICON=resource_path('gem.png')
+
+
+
 
 #---display shaped dictionary for easy assignment from shape name
 allShapes={}
@@ -36,16 +77,7 @@ allShapes['decagon']=DECAGON
 
 
 
-#---sghapes for shape select widget buttons
-CIRCLEBUTTON=("static/shapes/circleButton.png")
-SQUAREBUTTON=("static/shapes/squareButton.png")
-TRIANGLEBUTTON =("static/shapes/triangleButton.png")
-PENTAGONBUTTON=("static/shapes/pentagonButton.png")
-HEXAGONBUTTON =("static/shapes/hexagonButton.png")
-HEPTAGONBUTTON=("static/shapes/heptagonButton.png")
-OCTAGONBUTTON=("static/shapes/octagonButton.png")
-NONAGONBUTTON =("static/shapes/nonagonButton.png")
-DECAGONBUTTON=("static/shapes/decagonButton.png")
+
 
 #---button shapes dict for easy assignment from shape name
 allButtonShapes={}
@@ -59,16 +91,3 @@ allButtonShapes['octagon']=OCTAGONBUTTON
 allButtonShapes['nonagon']=NONAGONBUTTON
 allButtonShapes['decagon']=DECAGONBUTTON
 
-#---resource paths for images for compiled build
-
-# CIRCLE = resource_path('circle.png')
-# SQUARE = resource_path('square.png')
-# TRIANGLE = resource_path('triangle.png')
-# PENTAGON = resource_path('pentagon.png')
-# HEXAGON = resource_path('hexagon.png')
-# HEPTAGON = resource_path('heptagon.png')
-# OCTAGON = resource_path('octagon.png')
-# NONAGON = resource_path('nonagon.png')
-# DECAGON = resource_path('decagon.png')
-# KAARTICON = resource_path('Kaart.png')
-# GEMICON = resource_path('GEM3.png')
